58258e771a73b9b86805ecc6204ef6a4c3f251cc
Lochan Melam <53132118+LochanMelam@users.noreply.github.com>
2019-11-01 19:51:34 +0530

    Update projecteular-1.py

--- Task_13/projecteular-1.py
+++ Task_13/projecteular-1.py
@@ -2,7 +2,7 @@ t= int(input()) #gets number of test cases
 for i in range(t):
 	sum=0
 	k=int(input())
-	for j in range(1,k): #checking for multiples below 5 and 3
+	for j in range(1,k): #checking for multiples of 5 and 3 below 10
 			if j%3==0 or j%5==0:
 				sum=sum+j
 	print(sum)
