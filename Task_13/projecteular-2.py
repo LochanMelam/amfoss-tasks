5d44779510232ca118bdd4d7ed3bc537f5cb7b1d
Lochan Melam <53132118+LochanMelam@users.noreply.github.com>
2019-11-04 21:46:56 +0530

    Update projecteular-2.py

--- Task_13/projecteular-2.py
+++ Task_13/projecteular-2.py
@@ -2,13 +2,15 @@ t=int(input()) #number of testcases given
 a=0
 b=1
 c=0
+d=0
 for i in range(t):
 	n=int(input()) #sum of even fibonacci number upto n
 	while a<n:
 		if  a%2==0:
 			c=c+a
-		a=a+b
-		b=a+b
-	print(c)
+		a=b  #first term
+		b=d  #second term
+		d=a+b 
+	print(c) 
 
 
