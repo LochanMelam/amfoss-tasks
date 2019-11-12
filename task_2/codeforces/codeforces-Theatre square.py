a49b0aeb91164505564a9139859d4e2e644762a9
Lochan Melam <53132118+LochanMelam@users.noreply.github.com>
2019-11-12 19:27:28 +0530

    Update codeforces-Theatre square.py

--- task_2/codeforces/codeforces-Theatre square.py
+++ task_2/codeforces/codeforces-Theatre square.py
@@ -1,6 +1,10 @@
-n=list(map(int,input().split()))
-m=n[0]
-for i in range(0,len(n)):
-    if m>n[i]:
-        m=n[i]
-print(m)
+n,m,a=map(int,input().split())
+if m%a==0:
+    f=m//a
+else:
+    f=m//a+1
+if n%a==0:
+    d=n//a
+else:
+    d=n//a+1
+print(f*d)
